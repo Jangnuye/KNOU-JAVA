1f7d1c5e6bbfba59c910a6d09b42878be2360f0b
nuye <103883109+Jangnuye@users.noreply.github.com>
2022-05-29 16:12:27 +0900

    [Unit1] Java와 객체시향 프로그래밍
    - Java 클래스를 이해하기 위한 성적 클래스 작성
    - 성적 클래스를 활용한 성적처리 프로그램 제작

--- GradeOutput.java
+++ GradeOutput.java
@@ -1,24 +1,14 @@
 public class GradeOutput {
-        int e;          // for english grade variable
-        int m;          // for math grade variable
-
-        void input_grade(int a, int b) {        // input grade method
-        e = a;
-        m = b;
-    }
-
-        void output_grade() {               // output total grade method
-        System.out.println(e+m);
-    }
-
-        public static void main(String args[]) {
-        Grade g1, g2;       // object representing two grades
-        g1 = new Grade();   // make variable
+    public static void main(String args[])
+    {
+        Grade g1, g2;
+        g1 = new Grade();
         g2 = new Grade();
-        g1.input_grade(90, 85);     // input grade
-        g2.input_grade(80, 80);
+        g1.e = 90; g1.m = 85;
+        g2.e = 80; g2.m = 80;
 
-        g1.output_grade();      // output total grade
+        g1.output_grade();
         g2.output_grade();
+
     }
 }
