c033a41d33c837f4bdc628ba2a6845b5161ad633
nuye <103883109+Jangnuye@users.noreply.github.com>
2022-06-06 18:18:27 +0900

    [Unit 6] 패키지와 예외처리
    - 패키지 정의 방법을 이해
    - 패키지 사용 방법 익히기
    - 예외의 종류에 대해 이해
    - 예외 처리 방법을 이해

---
+++ Bus.java
@@ -0,0 +1,5 @@
+package com.vehicle;
+
+public class Bus extends com.vehicle.Car {
+    private String szType = "버스";
+}
---
+++ Truck.java
@@ -0,0 +1,5 @@
+package com.vehicle;
+
+public class Truck extends com.vehicle.Car {
+    private String szType = "트럭";
+}
